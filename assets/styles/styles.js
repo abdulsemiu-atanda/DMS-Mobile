ad24dfac1e631d84b6b9823fe14e1f3c91cfe783
Abdulsemiu Atanda <semiu@live.com>
2018-03-27 23:34:51 +0100

    layout signin page

--- assets/styles/styles.js
+++ assets/styles/styles.js
@@ -1,11 +1,9 @@
 import {StyleSheet} from 'react-native'
 
-export default styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F7F7F7',
   },
   welcome: {
     fontSize: 20,
@@ -16,5 +14,52 @@ export default styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  }
+})
+
+export const loginStyles = StyleSheet.create({
+  button: {
+    backgroundColor: '#0983ff',
+    height: 40,
+    alignItems: 'center',
+    marginTop: 20,
+    padding: 7
   },
+  buttonText: {
+    color: '#aacfff',
+    fontSize: 18
+  },
+  form: {
+    padding: 30
+  },
+  input: {
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    marginTop: 10,
+    marginBottom: 10
+  },
+  label: {
+    color: '#96a1ae',
+    fontSize: 10
+  },
+  logoContainer: {
+    width: '100%',
+    height: '52%',
+    backgroundColor: '#01f0b3'
+  },
+  infoContainer: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    padding: 30
+  },
+  infoText: {
+    color: '#96a1ae',
+    fontSize: 12,
+    fontWeight: '600',
+    marginRight: 5
+  },
+  signUpText: {
+    fontSize: 12,
+    fontWeight: '700'
+  }
 })
--- components/Home.react.js
+++ components/Home.react.js
@@ -5,10 +5,13 @@ import {
   Platform,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
   View
 } from 'react-native';
 
-import styles from '../assets/styles/styles'
+import {styles, loginStyles} from '../assets/styles/styles'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -22,15 +25,22 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={loginStyles.logoContainer}></View>
+        <View style={loginStyles.form}>
+          <Text style={loginStyles.label}>EMAIL</Text>
+          <TextInput keyboardType='email-address' style={loginStyles.input} placeholder='jason@bourne.com' />
+          <Text style={loginStyles.label}>PASSWORD</Text>
+          <TextInput secureTextEntry style={loginStyles.input} />
+          <TouchableHighlight style={loginStyles.button}>
+            <Text style={loginStyles.buttonText}>Sign In</Text>
+          </TouchableHighlight>
+        </View>
+        <View style={loginStyles.infoContainer}>
+          <Text style={loginStyles.infoText}>DON'T HAVE AN ACCOUNT?</Text>
+          <TouchableOpacity>
+            <Text style={loginStyles.signUpText}>SIGN UP</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
