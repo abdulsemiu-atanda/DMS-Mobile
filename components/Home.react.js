1341810ffb4db69d992cc26eea6e8b554563ece1
Abdulsemiu Atanda <semiu@live.com>
2018-03-28 01:14:15 +0100

    update styles for device agnostic compatibility

--- components/Home.react.js
+++ components/Home.react.js
@@ -82,7 +82,7 @@ export default class App extends Component {
             <Icon style={loginStyles.icon} name='ios-briefcase-outline' size={100} color='#f7f7f7' />
             <Text style={loginStyles.phrase}>Manage your documents elegantly.</Text>
           </View>
-          <View style={loginStyles.form}>
+          <View style={[loginStyles.form, {height: '35%'}]}>
             <Text style={loginStyles.label}>EMAIL</Text>
             <TextInput autoCapitalize='none' keyboardType='email-address' style={loginStyles.input} placeholder='jason@bourne.com' />
             <Text style={loginStyles.label}>PASSWORD</Text>
@@ -91,7 +91,7 @@ export default class App extends Component {
               <Text style={loginStyles.buttonText}>Sign In</Text>
             </TouchableHighlight>
           </View>
-          <View style={loginStyles.infoContainer}>
+          <View style={[loginStyles.infoContainer, {height: '16%'}]}>
             <Text style={loginStyles.infoText}>DON'T HAVE AN ACCOUNT?</Text>
             <TouchableOpacity onPress={this.flipCard}>
               <Text style={loginStyles.signUpText}>SIGN UP</Text>
